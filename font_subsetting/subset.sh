fe7e7c5438bfa3c4f3f00a84fb9cd89a96e0c8d4
Ene <1822216895@qq.com>
2025-04-11 18:44:56 +0800

    modify subset.sh

--- font_subsetting/subset.sh
+++ font_subsetting/subset.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # 检查参数数量
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
     echo "Usage: $0 <input_font.ttf> <output_font.woff2>"
     exit 1
 fi
 
 # 参数赋值
 INPUT_FONT="$1"
-OUTPUT_FONT="$2"
 SITE_DIR="../site"
 CHAR_FILE="./used_chars.txt"
 
@@ -25,7 +24,7 @@ grep -rh --include='*.html' --include='*.js' --include='*.css' '' "$SITE_DIR" | 
 # 执行字体子集化
 pyftsubset "$INPUT_FONT" \
           --text-file="$CHAR_FILE" \
-          --output-file="$OUTPUT_FONT" \
+          --output-file="$INPUT_FONT" \
           --flavor=woff2 \
           --recommended-glyphs \
           --layout-features='*' \
@@ -38,7 +37,7 @@ pyftsubset "$INPUT_FONT" \
 
 # 检查执行结果
 if [ $? -eq 0 ]; then
-    echo "字体子集化成功！输出文件：$OUTPUT_FONT"
+    echo "字体子集化成功！输出文件：$INPUT_FONT"
 else
     echo "字体子集化失败！"
     exit 1
